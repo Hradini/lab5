de18ae138bed03a9cb66316ed9dbda8ee63cc3eb
Hradini <31266306+Hradini@users.noreply.github.com>
2017-10-10 18:26:52 +0530

    Update q1.cpp

--- q1.cpp
+++ q1.cpp
@@ -1,36 +1,34 @@
 #include <iostream>
 using namespace std;
-
-int inputarr(int arr1[5]){
-	int i=0;
-	while(i < 5){
-	cout << "enter the " <<	i+1 << " th element";
-	cin >> arr1[i];
-	i++;
-	}
-}
-int findsum(int arr1[5]){
+int findsum(int arr1[])
+{
 	int sum =0;
 	int k=0;
 	while (k<5){
 		sum += arr1[k];
-		k++;
+		k++;	
 	}
-	
 	return sum;
 }
-int main(){
-int arr1[5];
-	inputarr(arr1);
-	int j=0;
+int main()
+{
+	int arr1[5];
+	int i=0;
+	while(i < 5)
+	{
+	cout << "enter the " <<	i+1 << " th element";
+	cin >> arr1[i];
+	i++;
+	}
+	i=0;
 	cout << "the elements of the array are ";
-	while(j<5){
-		cout << arr1[j] << " ";
-		j++;
+	while(i<5){
+		cout << arr1[i]<<" ";
+		i++;
 	}
 	int sum;
 	sum = findsum(arr1);
-	cout << sum;
+	cout << "sum "<<sum;
 	
 	return 0;
 }
